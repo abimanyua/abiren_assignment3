4496e69f42bb1a13f7c582c6a4b331471342a872
abimanyua <124319717+abimanyua@users.noreply.github.com>
2023-02-22 14:03:46 +0900

    Initial commit

---
+++ abiren_assignment3.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+# Assignment 3
+
+def gradientDescentFunction(data, iterations, learningRate):
+    i = 0
+
+
+# Question a)
+print("Question a: Please check the code above \n")
+
+# Question b)
+print("Question b:  Generate some random data, and estimate b using gradient descent algorithm\n")
+n = 10
+k = 2
+beta = np.array([1, 2, 5])
+X = np.random.randn(n, k)
+X = np.hstack((np.ones((n, 1)), X))
+noise = np.random.randn(n)
+Y = X.dot(beta) + noise
+# print(Y[9])
+
