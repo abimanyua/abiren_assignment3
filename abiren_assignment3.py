b9e7c961f09379fbb72978c78b98713b1cb2b033
abimanyua <124319717+abimanyua@users.noreply.github.com>
2023-02-27 15:21:55 +0900

    just added print

--- abiren_assignment3.py
+++ abiren_assignment3.py
@@ -1,11 +1,41 @@
 import numpy as np
 
-#test
-
 # Assignment 3
 
+#
 def gradientDescentFunction(data, iterations, learningRate):
-    i = 0
+    #coefficients = np.zeros(data.shape[1]-1)
+    num_features =  len(data[0]) - 1
+    coefficients = [0.0] * num_features
+
+    X = data[:, :-1]
+    y = data[:, -1]
+
+    for i in range(iterations):
+
+        # y_pred = np.dot(X, coefficients)
+        y_pred = []
+        for row in X:
+            pred = 0.0
+            for j in range(num_features):
+                pred += coefficients[j] * row[j]
+            y_pred.append(pred)
+
+        # error = y_pred - y
+        error = [y_pred[i] - y[i] for i in range(len(y))]
+
+        # gradient = np.dot(X.T, error) / len(y)
+        gradient = [0.0] * num_features
+        for j in range(num_features):
+            for i in range(len(y)):
+                gradient[j] += error[i] * X[i][j]
+            gradient[j] /= len(y)
+
+        # coefficients -= learningRate * gradient
+        for j in range(num_features):
+            coefficients[j] -= learningRate * gradient[j]
+
+    return coefficients
 
 
 # Question a)
@@ -21,5 +51,11 @@ X = np.random.randn(n, k)
 X = np.hstack((np.ones((n, 1)), X))
 noise = np.random.randn(n)
 Y = X.dot(beta) + noise
+# print(X)
+# print(Y)
 # print(Y[9])
 
+#
+
+result = gradientDescentFunction(X, 100, 0.0001)
+print(result)
