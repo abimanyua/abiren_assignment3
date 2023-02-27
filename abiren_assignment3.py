5a84ef2e99df8f4da9e8fb2e17fa4c8c6f9d6a2a
abimanyua <124319717+abimanyua@users.noreply.github.com>
2023-02-27 17:59:03 +0900

    Defined GDF function. Code works now.

--- abiren_assignment3.py
+++ abiren_assignment3.py
@@ -1,21 +1,28 @@
-import numpy as np
 import random
 
 # Assignment 3
 
-#
 def gradientDescentFunction(data, iterations, learningRate):
-    a = 0
-
+    X = [d[:-1] for d in data]
+    Y = [d[-1] for d in data]
+    n = len(Y)
+    k = len(X[0])
+    coefficients = [0] * k
+    for i in range(iterations):
+        predictions = [sum([X[j][p] * coefficients[p] for p in range(k)]) for j in range(n)]
+        errors = [predictions[j] - Y[j] for j in range(n)]
+        gradients = [2 * sum([X[j][p] * errors[j] for j in range(n)]) / n for p in range(k)]
+        coefficients = [coefficients[p] - learningRate * gradients[p] for p in range(k)]
+    return coefficients
 
 
 # Question a)
-print("Question a: Please check the code above \n")
+print("Question a: Estimate the regression coefficients 𝒃 by applying the gradient descent algorithm with Python.  \n")
 print(">> Please check the codes", "\n\n")
 
 # Question b)
 print("Question b:  Generate some random data, and estimate b using gradient descent algorithm\n")
-n = 1000
+n = 2000
 k = 2
 beta = [1, 2, 5]
 X = [[random.gauss(0, 1) for j in range(k)] for i in range(n)]
@@ -29,7 +36,7 @@ data = [X[i] + [Y[i]] for i in range(n)]
 # print(Y[9])
 
 iterations = 1000
-learningRate = 0.0001
+learningRate = 0.01
 
 result = gradientDescentFunction(data, iterations, learningRate)
-print(result)
+print(">>", result)
