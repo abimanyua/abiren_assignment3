741e3500dce0e680d6a87093fa2488f4a966ca9b
abimanyua <124319717+abimanyua@users.noreply.github.com>
2023-02-27 16:36:21 +0900

    Made changes to data generation to use without numpy

--- abiren_assignment3.py
+++ abiren_assignment3.py
@@ -1,41 +1,12 @@
 import numpy as np
+import random
 
 # Assignment 3
 
 #
 def gradientDescentFunction(data, iterations, learningRate):
-    #coefficients = np.zeros(data.shape[1]-1)
-    num_features =  len(data[0]) - 1
-    coefficients = [0.0] * num_features
+    a = 0
 
-    X = data[:, :-1]
-    y = data[:, -1]
-
-    for i in range(iterations):
-
-        # y_pred = np.dot(X, coefficients)
-        y_pred = []
-        for row in X:
-            pred = 0.0
-            for j in range(num_features):
-                pred += coefficients[j] * row[j]
-            y_pred.append(pred)
-
-        # error = y_pred - y
-        error = [y_pred[i] - y[i] for i in range(len(y))]
-
-        # gradient = np.dot(X.T, error) / len(y)
-        gradient = [0.0] * num_features
-        for j in range(num_features):
-            for i in range(len(y)):
-                gradient[j] += error[i] * X[i][j]
-            gradient[j] /= len(y)
-
-        # coefficients -= learningRate * gradient
-        for j in range(num_features):
-            coefficients[j] -= learningRate * gradient[j]
-
-    return coefficients
 
 
 # Question a)
@@ -44,13 +15,15 @@ print(">> Please check the codes", "\n\n")
 
 # Question b)
 print("Question b:  Generate some random data, and estimate b using gradient descent algorithm\n")
-n = 10
+n = 1000
 k = 2
-beta = np.array([1, 2, 5])
-X = np.random.randn(n, k)
-X = np.hstack((np.ones((n, 1)), X))
-noise = np.random.randn(n)
-Y = X.dot(beta) + noise
+beta = [1, 2, 5]
+X = [[random.gauss(0, 1) for j in range(k)] for i in range(n)]
+for i in range(n):
+    X[i] = [1] + X[i]
+noise = [random.gauss(0, 1) for i in range(n)]
+Y = [sum([X[i][j]*beta[j] for j in range(k+1)]) + noise[i] for i in range(n)]
+data = [X[i] + [Y[i]] for i in range(n)]
 # print(X)
 # print(Y)
 # print(Y[9])
